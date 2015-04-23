172a1d48b46deb1d191d4c4630e16bc96bb5794b
Gail Harmata <gail.harmata@gmail.com>
2015-04-22 20:55:18 -0500

    first draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## NOTE: I would like to thank Daniele Pigni for the helpful explanation of the
+## examples for this assignment.  The explanation can be found at
+## https://class.coursera.org/rprog-013/forum/thread?thread_id=694.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix defines four functions: set, get, setmatrix, and
+## getmatrix. In order to access these functions, one needs to use use the '$'
+## symbol as used in subsetting: makeCacheMatrix$get, etc.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+     x <<- y
+     m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+}
 }
 
 
