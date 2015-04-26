3787553f7984a7b52c24f10df8dbf3ff453aa4aa
Gail Harmata <gail.harmata@gmail.com>
2015-04-25 22:14:52 -0500

    Removed singular matrix check
    Removed the if/else statement checking for matrices without inverses,
    as the assignment was to assume that the matrices could be inverted.
    
    Also edited some of the comments to match the changes to the code and
    improve readability.

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,26 @@
-# NOTE: I would like to thank Daniele Pigni for the helpful explanation of the
-# examples for this assignment.  The explanation can be found at
-# https://class.coursera.org/rprog-013/forum/thread?thread_id=694.
+## NOTE: I would like to thank Daniele Pigni for the helpful explanation of the
+## examples for this assignment.  The explanation can be found at
+## https://class.coursera.org/rprog-013/forum/thread?thread_id=694.
 
 ##############################################################
 
 ## Function 1: makeCacheMatrix
 
 ## The function makeCacheMatrix defines four functions: set, get, setmatrix, and
-## getmatrix. These sub-functions allow the 
-## In order to access these functions, one needs to use use the '$'
-## symbol as used in subsetting: makeCacheMatrix(x)$get(), etc. 
+## getmatrix. These sub-functions allow the user to set a matrix or its inverse,
+## and to retrieve the previously cached matrix or inverse. 
+
+## TIP: In order to access these functions, one needs to use use the '$' symbol as used in subsetting:
+## makeCacheMatrix(x)$get(), etc. The command is less lengthy if one assigns
+## makeCacheMatrix(x) to a new variable z in the console, so that one can use
+## the command z$get(), etc.
 
 makeCacheMatrix <- function(x = matrix()) {
   
-   #Part 1: Creates an empty variable i.
+   # Part 1: Creates an empty variable i.
    i <- NULL
    
-   #Part 2: The set, get, setinverse, and getinverse functions
+   # Part 2: The set, get, setinverse, and getinverse functions
    set <- function(y) {
       x <<- y
       i <<- NULL
@@ -33,13 +37,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
+
 ## Function 2: cacheSolve
 
 ## The function cacheSolve first checks to see if the inverse of a matrix x has
 ## already been calculated and cached.  If it has, the function retrieves the
-## inverse matrix; if not, the function checks to see if the matrix has an
-## inverse (det != 0).  If it does, the function calculates the inverse of the
-## matrix, caches it for later use, and returns it.
+## inverse matrix; if not, the function calculates the inverse of the
+## matrix, caches it for later use, and displays it.
 
 cacheSolve <- function(x, ...) {
    
@@ -53,18 +57,10 @@ cacheSolve <- function(x, ...) {
    }
    data <- x$get()
    
-   # Part 2: Checks if matrix can be inverted, then solves for the inverse of
-   # the matrix if possible.  If the matrix does not have an inverse, a message
-   # saying so is returned.  For more information, check out 
-   # http://mathworld.wolfram.com/SingularMatrix.html or
-   # http://en.wikipedia.org/wiki/Invertible_matrix.
+   # Part 2: Solves for the inverse of the matrix. This part assumes that the 
+   # matrix is invertible, as per the assignment instructions.
    
-   if(det(data)!= 0){
       i <- solve(data, ...)
       x$setinverse(i)
       i
-   }
-   
-   else {
-      message("Provided matrix does not have an inverse; determinant = 0")}
 }
